03c1a0de90e2ca5e06f3c8c376ba920448b29bfd
Kenny Garcia <kennygarcia15@csu.fullerton.edu>
2025-09-06 11:59:24 -0700

    Added four more locations. Have a total of 6 locations right now thinking of adding four more.

--- riddles.py
+++ riddles.py
@@ -1,12 +1,29 @@
 RIDDLES = {
     # Replace these keys with your real Supabase station IDs (UUIDs)
-    "00000000-0000-0000-0000-000000000000": {
+    "e96d2104-0c34-45b6-88fc-3f075d55f349": {
         "seed": "Where whispers beat shouts and ideas stack in quiet towers.",
         "aliases": ["Library", "Langsdorf", "Pollak", "CSUF Library"],
     },
-    "11111111-1111-1111-1111-111111111111": {
+    "6e681c03-1914-4945-bc1d-f3f6de81387e": {
         "seed": "Follow the clang of trays and the hum of laptops where hunger takes a study break.",
-        "aliases": ["Cafeteria", "Food Court", "Dining"],
+        "aliases": ["Cafeteria", "Food Court", "Dining", "TSU"],
     },
+    "46ba7881-0999-4736-ae10-bc20b35dbd1c": {
+        "seed": "I am the hall where Titans chase strength not grades where the water runs indoors and the climb never reaches the sky what place am I",
+        "aliases": ["Studnet REC", "Gym", "Working out", "Rock climb"],
+    },
+    "ff9f5db7-c773-4886-ba3b-1f858e1d0ff9": {
+        "seed": "Seek the tower where stories and languages meet, and climb until no more stairs remain to find your prize.",
+        "aliases": ["Humanities", "Heightest building", "Tallest building"],
+    },
+    "5eccd0a7-95ed-417e-93d2-4c1e4c3c8ec3": {
+        "seed": "In this tower of steel and stone where carriages rest unseen by Muggles, ascend nearly to the top, then seek the fourth pillar as if it were the gateway to Platform Nine and Three Quarters.",
+        "aliases": ["Nutwood", "Parking structure", "Parking lot", "Parking", "Cars",],
+    },
+    "a89824c8-5db5-4a82-b644-591aed7a9f4d": {
+        "seed": "Behind the walls of learning, a hidden cosmos waits where stone planets keep silent orbits; only those who look past the building’s shadow will find the universe on campus.",
+        "aliases": ["McCarthy", "Behind McCarthy", "McCarthy Building"],
+    }
+    
     # add more as you add real stations
 }
